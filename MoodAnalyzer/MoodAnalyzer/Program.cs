a24641423818b4179dc9593af4b90e36eda5f665
ronit#123 <ronitwaje23>
2022-02-13 21:57:44 +0530

    adding mood analyzer program

---
+++ MoodAnalyzer/MoodAnalyzer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to the mood analyzer program");
+            Console.ReadLine();
+        }
+    }
+}
