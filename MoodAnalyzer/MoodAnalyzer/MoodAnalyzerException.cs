23877813993da58644c145cbc8fc10bbb0df16ef
ronit#123 <ronitwaje23>
2022-02-14 18:51:30 +0530

    Handle Null And Empty Exception

---
+++ MoodAnalyzer/MoodAnalyzer/MoodAnalyzerException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MoodAnalyzer
+{
+    [Serializable]
+    public class MoodAnalyzerException : Exception
+    {
+        public ExceptionType type;
+        //Enumeration
+        public enum ExceptionType
+        {
+            NULL_EXCEPTION,
+            EMPTY_EXCEPTION
+        }
+        //Exception Constructor.
+        public MoodAnalyzerException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
